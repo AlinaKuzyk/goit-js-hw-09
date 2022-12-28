7ca1c105aefec0c7883c23533606101aad56d3bb
AlinaKuzyk <dr.alinakuzyk@gmail.com>
2022-12-28 17:33:42 +0000

    Deploying to gh-pages from @ AlinaKuzyk/goit-js-hw-09@2b62d752c21b6f9fb842d6d8a56c3d8bd0fb4818 🚀

---
+++ 03-promises.7042e00c.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".form");var e=document.querySelector("button");console.log("asd"),e.addEventListener("submit",(function(e){e.preventDefault();var t=e.target.elements;console.log(t)}))}();
+//# sourceMappingURL=03-promises.7042e00c.js.map
